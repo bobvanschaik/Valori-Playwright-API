1ebe6a9de0f62a0b643a1b569799402c29351b5f
bobvanschaik <bobvanschaik@gmail.com>
2024-03-18 19:14:09 +0100

    Update tv-maze.spec.ts

--- tests/tv-maze.spec.ts
+++ tests/tv-maze.spec.ts
@@ -1,2 +1,8 @@
 import { APIRequestContext, test, expect } from '@playwright/test';
-import { StatusCodes } from 'http-status-codes';
+import { TvMazeClient } from '../apis/tv-maze/tv-maze-client';
+
+test('Checkid', async ({ TvMazeClient }) => {
+    const response = await TvMazeClient;
+    const body = await response.text();
+    expect(response.status()).toBe(StatusCodes.OK);
+});
